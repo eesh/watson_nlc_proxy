194912079dc54d7bc1998217e58b687753d8781b
Eesh Chilukalapalli <eeshlikhith@gmail.com>
2018-03-28 21:27:26 +0530

    First push

---
+++ index.js
@@ -0,0 +1,49 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+let app = express()
+let cors = require('cors')
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+let NaturalLanguageClassifierV1 = require('watson-developer-cloud/natural-language-classifier/v1');
+
+var naturalLanguageClassifier = new NaturalLanguageClassifierV1({
+  username: '',
+  password: ''
+});
+
+app.post('/classify', function (req, res) {
+  if(req.body.auth_pass == null || req.body.auth_user == null) {
+    res.json({ error: 'Auth info not supplied' })
+    return
+  }
+  if(req.body.text == null) {
+    res.json({ error : 'Text not supplied' })
+    return
+  }
+  if(req.body.classifier_id == null) {
+    res.json({ error : 'classifier_id not supplied' })
+    return
+  }
+  var naturalLanguageClassifier = new NaturalLanguageClassifierV1({
+    username: req.body.auth_user,
+    password: req.body.auth_pass
+  });
+  naturalLanguageClassifier.classify(
+  {
+    text: req.body.text,
+    classifier_id: req.body.classifier_id
+  },
+  function(err, response) {
+    if(err) {
+      console.log(err.message)
+      res.json(err)
+    } else {
+      console.log(response)
+      res.json(response)
+    }
+  });
+})
+
+app.listen(5000)
