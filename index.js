c30c47a41e985da44bd952f477cd1af63103751d
Eesh Chilukalapalli <eeshlikhith@gmail.com>
2018-05-07 00:21:05 +0530

    integrate twitter api

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const fs = require('fs')
 const watson_app = require('./watson_app')
 const alexa_app = require('./alexa_app')
+const twitter_app = require('./twitter_app')
 const https = require('https')
 const socketManager = require('./socketManager')
 const args = require('minimist')(process.argv.slice(2))
 
 let WATSON_HTTP_PORT = 3477
 let ALEXA_HTTP_PORT = 6456
+let TWITTER_HTTP_PORT = 3276
+
 
 if(args.http == true) {
 
@@ -18,6 +21,10 @@ if(args.http == true) {
     console.log(`Alexa server running on port ${ALEXA_HTTP_PORT}`)
   })
 
+
+  twitter_app.listen(TWITTER_HTTP_PORT, () => {
+    console.log(`Twitter server running on port ${TWITTER_HTTP_PORT}`)
+  })
 } else {
 
 
@@ -38,5 +45,10 @@ if(args.http == true) {
   socketManager.initialize(alexa_server)
   alexa_server.listen(ALEXA_HTTP_PORT, () => {
     console.log(`Alexa server running on port ${ALEXA_HTTP_PORT}`)
+  })
+
+  var twitter_server = require('https').createServer(options, twitter_app);
+  twitter_server.listen(TWITTER_HTTP_PORT, () => {
+    console.log(`Twitter server running on port ${TWITTER_HTTP_PORT}`)
   })
 }
